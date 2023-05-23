2546d3f5b56cce0927a1112103257707c2cd650e
Pravin Raj Paul <31380840+pravinrajpaul@users.noreply.github.com>
2023-05-23 22:49:53 +0530

    Add files via upload

---
+++ jenkins-katalon.sh
@@ -0,0 +1,24 @@
+sudo apt install curl
+sudo apt-get install openjdk-11-jdk
+sudo apt-get install openjdk-11-jre
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+docker pull katalonstudio/katalon
+sudo cd /etc/environment
+sudo echo "export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))" >> .bashrc
+sudo echo "export PATH=$PATH:$JAVA_HOME/bin" >> .bashrc
+sudo apt-get install git
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins.service
